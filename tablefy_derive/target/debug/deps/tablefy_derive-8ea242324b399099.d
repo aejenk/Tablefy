ec2ad2c71204f17ab3620ee899b018b9c38edf59
Calmynt <mctech26@gmail.com>
2019-07-25 23:41:48 +0200

    Removed target folders

--- tablefy_derive/target/debug/deps/tablefy_derive-8ea242324b399099.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Textman\Desktop\Rust\tablefy\tablefy-derive\target\debug\deps\tablefy_derive-8ea242324b399099.dll: src\lib.rs
-
-C:\Users\Textman\Desktop\Rust\tablefy\tablefy-derive\target\debug\deps\tablefy_derive-8ea242324b399099.d: src\lib.rs
-
-src\lib.rs:
--- tablefy_derive/target/debug/tablefy_derive.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Textman\Desktop\Rust\tablefy\tablefy-derive\target\debug\tablefy_derive.dll: C:\Users\Textman\Desktop\Rust\tablefy\tablefy-derive\src\lib.rs
