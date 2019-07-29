065c8ac6c7bd64cca1dbb4407d993a54d035b7b3
Calmynt <mctech26@gmail.com>
2019-07-29 21:30:30 +0200

    Added prototype for header name attribute
    Currently the attribute doesn't work, and the compiler fails to build
    with the error #29642. Until this issue is either circumvented or
    solved, making this attribute available will not be possible.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use tablefy_derive::Tablefy;
+use tablefy::Tablefy;
+
+// This struct now implements the tablefy trait
+#[derive(Tablefy)]
+pub struct Basic {
+
+    #[header(name = "custom")]
+    pub something: String,
+    pub otherthing: i16,
+    pub newthing: i8,
+    pub maybe: Option<String>
+}
+
+fn main() {
+    // Creating a vector of structs...
+    let basic = vec![Basic {
+        something: String::from("a"),
+        otherthing: 2,
+        newthing: 3,
+        maybe: None
+    }, Basic {
+        something: String::from("b"),
+        otherthing: 3,
+        newthing: 4,
+        maybe: Some(String::from("x"))
+    }, Basic {
+        something: String::from("c"),
+        otherthing: 5,
+        newthing: 8,
+        maybe: None
+    }];
+
+    // Turning them into a Table struct...
+    let table = tablefy::into_table(&basic);
+
+    // Or if you just want the string...
+    println!("{}", tablefy::into_string(&basic));
+}
